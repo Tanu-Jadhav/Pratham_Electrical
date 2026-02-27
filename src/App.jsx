4a3569c88bb1531de62a19531700d38df25361ba
Tanuja Bramhadev Jadhav <tanujadhav2207@gmail.com>
2026-02-27 16:33:24 +0530

    cean setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
-import { Routes, Route } from "react-router-dom";
-
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-
 import Home from "./pages/Home";
-import GalleryPage from "./pages/GalleryPage";
 
 function App() {
   return (
     <>
       <Navbar />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/*" element={<Home />} />
-        <Route path="/gallery" element={<GalleryPage />} />
-      </Routes>
-
+      <Home />
       <Footer />
     </>
   );
